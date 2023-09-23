ac3a6783f05b0d91d606f5930b3e47cb16fb577e
Bandepalli Surya Anjani Kumar <89148170+Surya-Kumar-03@users.noreply.github.com>
2023-09-23 22:31:22 +0530

    Update three.c

--- 3. Intermediate Code Generation/three.c
+++ 3. Intermediate Code Generation/three.c
@@ -4,21 +4,42 @@ void pm();
 void plus();
 void div();
 int i, ch, j, l, addr = 100;
-char ex[10], exp[10], exp1[10], exp3[10], id1[5], op[5], id2[5];
-void main()
+char ex[10], exp0[10], exp1[10], exp3[10], id1[5], op[5], id2[5];
+
+void strrev(char *str)
+{
+    if (!str)
+    {
+        return;
+    }
+
+    int i = 0;
+    int j = strlen(str) - 1;
+
+    while (i < j)
+    {
+        char c = str[i];
+        str[i] = str[j];
+        str[j] = c;
+        i++;
+        j--;
+    }
+}
+
+int main()
 {
     // clrscr();
     printf("\nEnter the expression with arithmetic operator:");
     scanf("%s", ex);
-    strcpy(exp, ex);
-    l = strlen(exp);
+    strcpy(exp0, ex);
+    l = strlen(exp0);
     exp1[0] = '\0';
 
     for (i = 0; i < l; i++)
     {
-        if (exp[i] == '+' || exp[i] == '-')
+        if (exp0[i] == '+' || exp0[i] == '-')
         {
-            if (exp[i + 2] == '/' || exp[i + 2] == '*')
+            if (exp0[i + 2] == '/' || exp0[i + 2] == '*')
             {
                 pm();
                 break;
@@ -29,31 +50,34 @@ void main()
                 break;
             }
         }
-        else if (exp[i] == '/' || exp[i] == '*')
+        else if (exp0[i] == '/' || exp0[i] == '*')
         {
             div();
             break;
         }
     }
-    getch();
+    // getch();
 }
+
 void pm()
 {
-    strrev(exp);
+    strrev(exp0);
     j = l - i - 1;
-    strncat(exp1, exp, j);
+    strncat(exp1, exp0, j);
     strrev(exp1);
-    printf("Three address code:\ntemp=%s\ntemp1=%c%ctemp\n", exp1, exp[j + 1], exp[j]);
+    printf("Three address code:\ntemp=%s\ntemp1=%c%ctemp\n", exp1, exp0[j + 1], exp0[j]);
 }
+
 void div()
 {
-    strncat(exp1, exp, i + 2);
-    printf("Three address code:\ntemp=%s\ntemp1=temp%c%c\n", exp1, exp[i + 2], exp[i + 3]);
+    strncat(exp1, exp0, i + 2);
+    printf("Three address code:\ntemp=%s\ntemp1=temp%c%c\n", exp1, exp0[i + 2], exp0[i + 3]);
 }
+
 void plus()
 {
-    strncat(exp1, exp, i + 2);
-    printf("Three address code:\ntemp=%s\ntemp1=temp%c%c\n", exp1, exp[i + 2], exp[i + 3]);
+    strncat(exp1, exp0, i + 2);
+    printf("Three address code:\ntemp=%s\ntemp1=temp%c%c\n", exp1, exp0[i + 2], exp0[i + 3]);
 }
 
 /*Sample Output:
@@ -62,4 +86,6 @@ Enter the expression with arithmetic operator:
 a+b-c
 Three address code:
 temp=a+b
-temp1=temp-c*/
+temp1=temp-c
+
+*/
